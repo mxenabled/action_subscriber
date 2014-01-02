0db5a799898cddeb289ecd9fbd12406b865907c1
Devin Christensen <quixoten@gmail.com>
2014-01-02 11:48:18 -0700

    Improve auto-recovery
    This change came about because the pause method is not available in the
    JRuby version of EventMachine.
    
    In my initial version, I overlooked the possibility of closing the
    EventMachine connection without using its instance methods (all of which
    prevented auto-recovery). Well, as it turns out, that was idiotic. This
    method is more straightforward, cleaner, faster, and more reliable. That
    I missed it the first time around is boggling.

--- lib/action_subscriber/rabbit_connection.rb
+++ lib/action_subscriber/rabbit_connection.rb
@@ -33,27 +33,16 @@ module ActionSubscriber
     end
 
     def self.setup_recovery
-      # When the server fails to respond to a heartbeat, we assume that it
-      # is dead and attempt to reconnect with auto recovery. To accomplish
-      # this, we must timeout the underlying EventMachine connection. If we
-      # reconnected manually, the auto recovery code would not be triggered,
-      #
-      # Forcing a timeout in EventMachine is a two step process:
-      #   1. Pause all activity on the connection
-      #   2. Configure the connection to timeout when there is no activity
+      # When the server fails to respond to a heartbeat, it is assumed
+      # to be dead. By closing the underlying EventMachine connection,
+      # a connection loss is triggered in AMQP .
       self.connection.on_skipped_heartbeats do |session|
-        # This completes step 1
-        session.pause
-
-        # This completes step 2. Remember that when we run this code a
-        # skipped heartbeat has already been detected. We want the
-        # timeout, and consequently auto recovery, to happen immediately
-        session.comm_inactivity_timeout = 0.01
+        EventMachine.close_connection(session.signature, false)
       end
 
+      # When the connection loss is triggered, we reconnect, which
+      # also runs the auto-recovery code
       self.connection.on_tcp_connection_loss do |session, settings|
-        # A reconnect also resets the inactivity timeout to 0.0 and
-        # resumes the connection
         session.reconnect(false, 1)
       end
     end
