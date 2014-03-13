fb11ecfeb7442ac9ae1d923087d633004a46a23f
Adam Hutchison <liveh2o@gmail.com>
2014-03-13 13:36:47 -0600

    Remove the base middleware class
    Having a base middleware class is odd and does not follow a typical
    middleware pattern. Remove it.

--- lib/action_subscriber/middleware/active_record_connection.rb
+++ lib/action_subscriber/middleware/active_record_connection.rb
@@ -1,6 +1,12 @@
 module ActionSubscriber
   module Middleware
-    class ActiveRecordConnection < Base
+    class ActiveRecordConnection
+      attr_reader :app
+
+      def initialize(app)
+        @app = app
+      end
+
       def call(subscriber)
         ::ActiveRecord::Base.connection_pool.with_connection do
           app.call(subscriber)
--- lib/action_subscriber/middleware/router.rb
+++ lib/action_subscriber/middleware/router.rb
@@ -1,6 +1,10 @@
 module ActionSubscriber
   module Middleware
-    class Router < Base
+    class Router
+      def initialize(app)
+        @app = app
+      end
+
       def call(subscriber)
         subscriber.consume_event
       end
