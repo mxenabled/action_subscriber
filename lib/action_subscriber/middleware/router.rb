ad9d044f325065ea1eb05c6ef49c104864ebed5e
Adam Hutchison <liveh2o@gmail.com>
2014-03-17 16:41:01 -0600

    Deprecate passing the payload to subscribable actions
    Since the payload is already in the subscriber, we should be using that
    instead of passing the payload to each action.

--- lib/action_subscriber/middleware/router.rb
+++ lib/action_subscriber/middleware/router.rb
@@ -10,7 +10,17 @@ module ActionSubscriber
 
         # TODO: Figure out if we should be checking this
         #
-        subscriber.__send__(env.action) if subscriber.respond_to?(env.action)
+        if subscriber.respond_to?(env.action)
+          action = subscriber.method(env.action)
+
+          # TODO: Remove this check in 1.0
+          if action.arity == 0
+            action.call
+          else
+            warn 'DEPRECATED passing the payload to subscribable methods is deprecated and will be removed in Action Subscriber 1.0. Use the :payload attribute in the subscriber instead.'
+            action.call(env.payload)
+          end
+        end
 
       # TODO: Extract error handling into a middleware
       #
