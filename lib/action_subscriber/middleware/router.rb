d292af829e46893125236b1db4be325831c527a1
Adam Hutchison <liveh2o@gmail.com>
2014-03-17 11:13:33 -0600

    Extract routing into a middleware
    Now the subscriber is initialized in the router middleware (e.g. the
    end of the line) and dispatches the action (e.g. calls ‘created’)
    instead of relying on the consume_event API.

--- lib/action_subscriber/middleware/router.rb
+++ lib/action_subscriber/middleware/router.rb
@@ -6,7 +6,16 @@ module ActionSubscriber
       end
 
       def call(env)
-        env.subscriber.consume_event
+        subscriber = env.subscriber.new(env)
+
+        # TODO: Figure out if we should be checking this
+        #
+        subscriber.__send__(env.action) if subscriber.respond_to?(env.action)
+
+      # TODO: Extract error handling into a middleware
+      #
+      rescue => exception
+        ::ActionSubscriber.configuration.error_handler.call(exception)
       end
     end
   end
