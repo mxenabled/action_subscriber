73b9cdc51375426cbe4bcb338a9bf0c44d045bed
Devin Christensen <quixoten@gmail.com>
2014-06-30 15:16:00 -0600

    Recover from closed connections

--- lib/action_subscriber/rabbit_connection.rb
+++ lib/action_subscriber/rabbit_connection.rb
@@ -2,12 +2,13 @@ module ActionSubscriber
   module RabbitConnection
     def self.bunny_connection_options
       {
-        :heartbeat                 => ::ActionSubscriber.configuration.heartbeat,
-        :host                      => ::ActionSubscriber.configuration.host,
-        :port                      => ::ActionSubscriber.configuration.port,
-        :continuation_timeout      => ::ActionSubscriber.configuration.timeout * 1_000.0, #convert sec to ms
-        :automatically_recover     => true,
-        :network_recovery_interval => 1,
+        :heartbeat                     => ::ActionSubscriber.configuration.heartbeat,
+        :host                          => ::ActionSubscriber.configuration.host,
+        :port                          => ::ActionSubscriber.configuration.port,
+        :continuation_timeout          => ::ActionSubscriber.configuration.timeout * 1_000.0, #convert sec to ms
+        :automatically_recover         => true,
+        :network_recovery_interval     => 1,
+        :recover_from_connection_close => true,
       }
     end
 
@@ -31,12 +32,12 @@ module ActionSubscriber
 
     def self.march_hare_connection_options
       {
-        :heartbeat_interval        => ::ActionSubscriber.configuration.heartbeat,
-        :host                      => ::ActionSubscriber.configuration.host,
-        :port                      => ::ActionSubscriber.configuration.port,
-        :continuation_timeout      => ::ActionSubscriber.configuration.timeout * 1_000.0, #convert sec to ms
-        :automatically_recover     => true,
-        :network_recovery_interval => 1,
+        :heartbeat_interval            => ::ActionSubscriber.configuration.heartbeat,
+        :host                          => ::ActionSubscriber.configuration.host,
+        :port                          => ::ActionSubscriber.configuration.port,
+        :continuation_timeout          => ::ActionSubscriber.configuration.timeout * 1_000.0, #convert sec to ms
+        :automatically_recover         => true,
+        :network_recovery_interval     => 1,
       }
     end
   end
