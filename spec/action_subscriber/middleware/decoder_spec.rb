d25dc70384711fc80aeba17ffb8475cb9152fadd
Michael Ries <michael@riesd.com>
2014-11-05 09:35:29 -0700

    fix the decoder_spec so it passes under jruby

--- spec/action_subscriber/middleware/decoder_spec.rb
+++ spec/action_subscriber/middleware/decoder_spec.rb
@@ -3,30 +3,29 @@ require 'spec_helper'
 describe ActionSubscriber::Middleware::Decoder do
   include_context 'action subscriber middleware env'
 
-  let(:encoded_payload) { env.encoded_payload }
-
   subject { described_class.new(app) }
 
   it_behaves_like 'an action subscriber middleware'
 
-  before { message_properties[:content_type] = "application/foo"}
+  let(:env) { ActionSubscriber::Middleware::Env.new(UserSubscriber, encoded_payload, message_properties) }
+  let(:encoded_payload) { JSON.generate(payload) }
+  let(:payload) { {"ohai" => "GUYZ"} }
 
   context "when the content type has an associated decoder" do
-    let(:decoder) { { 'application/foo' => lambda { |_| payload } } }
-    let(:payload) { double(:payload) }
-
-    before { allow(ActionSubscriber.config).to receive(:decoder).and_return(decoder) }
+    before { message_properties[:content_type] = "application/json"}
 
     it "decodes the payload" do
-      allow(env).to receive(:payload=).with(payload)
       subject.call(env)
+      expect(env.payload).to eq(payload)
     end
   end
 
   context "when the content type does not have an associated decoder" do
-    it "dups the payload" do
-      allow(env).to receive(:payload=).with(encoded_payload)
+    before { message_properties[:content_type] = "application/foo"}
+
+    it "uses the payload as-is" do
       subject.call(env)
+      expect(env.payload).to eq(encoded_payload)
     end
   end
 end
