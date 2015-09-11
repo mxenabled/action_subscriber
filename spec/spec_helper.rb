034fb924817bd7f081f16cd307a6bf0b9c5c3704
Michael Ries <michael@riesd.com>
2015-09-11 16:45:39 -0600

    helper method for verifying asynchronous results

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,3 +30,17 @@ RSpec.configure do |config|
     end
   end
 end
+
+def verify_expectation_within(number_of_seconds, check_every = 0.02)
+  waiting_since = ::Time.now
+  begin
+    sleep check_every
+    yield
+  rescue RSpec::Expectations::ExpectationNotMetError => e
+    if ::Time.now - waiting_since > number_of_seconds
+      raise e
+    else
+      retry
+    end
+  end
+end
