b0401cfd046bccde07249fb307fbad3210e3a71b
Brett Allred <brettallred@gmail.com>
2015-09-20 08:46:36 -0700

    change config file

--- lib/action_subscriber/configuration.rb
+++ lib/action_subscriber/configuration.rb
@@ -39,7 +39,7 @@ module ActionSubscriber
       yaml_config = {}
       absolute_config_path = ::File.expand_path(::File.join("config", "action_subscriber.yml"))
       if ::File.exists?(absolute_config_path)
-        yaml_config = ::YAML.load_file(babou_absolute_config_path, :safe => true)[env]
+        yaml_config = ::YAML.load_file(absolute_config_path, :safe => true)[env]
       end
 
       ::ActionSubscriber::Configuration::DEFAULTS.each_pair do |key, value|
