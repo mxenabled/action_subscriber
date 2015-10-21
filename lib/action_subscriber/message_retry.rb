f99c56cb669d2169a5efee620ccc924a5229f7b9
Michael Ries <michael@riesd.com>
2015-10-21 09:26:26 -0600

    use a custom attempt header to control schedule

--- lib/action_subscriber/message_retry.rb
+++ lib/action_subscriber/message_retry.rb
@@ -1,35 +1,44 @@
 module ActionSubscriber
   module MessageRetry
+    SCHEDULE = {
+      2  =>        100,
+      3  =>        500,
+      4  =>      2_500,
+      5  =>     12_500,
+      6  =>     62_500,
+      7  =>    312_500,
+      8  =>  1_562_500,
+      9  =>  7_812_500,
+      10 => 39_062_500,
+    }.freeze
+
     def self.redeliver_message_with_backoff(env)
-      ttl = calculate_ttl(env)
+      next_attempt = get_last_attempt_number(env) + 1
+      ttl = SCHEDULE[next_attempt]
       return unless ttl
       with_exchange(env, ttl) do |exchange|
-        exchange.publish(env.encoded_payload,
-          :content_type => env.content_type,
-          :routing_key => env.routing_key,
-          :expiration => ttl
-        )
+        exchange.publish(env.encoded_payload, retry_options(env, next_attempt))
       end
     end
 
     # Private Implementation
-    def self.calculate_ttl(env)
-      last_ttl = get_last_ttl_or_default(env.headers["x-death"], 20)
-      next_ttl = last_ttl * 5
-      return false if next_ttl > 86_400_000
-      next_ttl
+    def self.get_last_attempt_number(env)
+      attempt_header = env.headers.fetch("as-attempt", "1")
+      attempt_header.to_i
     end
 
-    def self.get_last_ttl_or_default(x_death, default)
-      return default unless x_death
-      x_death = parse_x_death(x_death) if x_death.is_a?(String)
-      x_death.map{|death| death["original-expiration"].to_i}.max
+    def self.retry_headers(env, attempt)
+      env.headers.reject do |key, val|
+        key == "x-death"
+      end.merge({"as-attempt" => attempt.to_s})
     end
 
-    def self.parse_x_death(x_death)
-      x_death.scan(/\{([^}]+)\}/).map(&:first).map do |str|
-        str.scan(/([a-z\-]+)=([0-9a-z\-_.]+)/).to_h
-      end
+    def self.retry_options(env, attempt)
+      {
+        :content_type => env.content_type,
+        :routing_key => env.routing_key,
+        :headers => retry_headers(env, attempt),
+      }
     end
 
     def self.with_exchange(env, ttl)
@@ -39,7 +48,7 @@ module ActionSubscriber
       begin
         channel.confirm_select
         exchange = channel.topic(exchange_retry_name)
-        queue = channel.queue(queue_retry_name, :arguments => {"x-dead-letter-exchange" => env.exchange})
+        queue = channel.queue(queue_retry_name, :arguments => {"x-dead-letter-exchange" => env.exchange, "x-message-ttl" => ttl})
         queue.bind(exchange, :routing_key => env.routing_key)
         yield(exchange)
         channel.wait_for_confirms
