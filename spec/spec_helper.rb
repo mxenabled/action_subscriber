2d67b1e0798699b939fe1ee6252a1c3ad11ac135
Michael Ries <michael@riesd.com>
2015-11-05 23:03:05 -0700

    update spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,9 +25,7 @@ RSpec.configure do |config|
   end
   config.after(:each, :integration => true) do
     ::ActionSubscriber::RabbitConnection.subscriber_disconnect!
-    ::ActionSubscriber::Base.inherited_classes.each do |klass|
-      klass.instance_variable_set("@_queues", nil)
-    end
+    ::ActionSubscriber.instance_variable_set("@route_set", nil)
   end
 end
 
