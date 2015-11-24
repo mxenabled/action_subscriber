3ff2b3f73e322747082d4cf96d0e31f226b84e93
Michael Ries <michael@riesd.com>
2015-11-24 15:23:43 -0700

    add more debug info on USR2 trap

--- lib/action_subscriber/threadpool.rb
+++ lib/action_subscriber/threadpool.rb
@@ -7,6 +7,10 @@ module ActionSubscriber
       (pool.pool_size == pool.busy_size)
     end
 
+    def self.number_of_jobs_queued
+      pool.instance_variable_get('@queued_jobs').size
+    end
+
     def self.perform_async(*args)
       self.pool.async.perform(*args)
     end
@@ -15,6 +19,16 @@ module ActionSubscriber
       @pool ||= ::Lifeguard::InfiniteThreadpool.new(
         :pool_size => ::ActionSubscriber.config.threadpool_size
       )
+    end
+
+    def self.print_threadpool_stackraces
+      $stderr.puts "ActionSubscriber :: Threadpool stacktraces\n"
+      pool.instance_variable_get('@busy_threads').each do |thread|
+        $stderr.puts <<-THREAD_TRACE
+          #{thread.inspect}:
+          #{thread.backtrace.try(:join, $INPUT_RECORD_SEPARATOR)}"
+        THREAD_TRACE
+      end
     end
 
     def self.ready?
