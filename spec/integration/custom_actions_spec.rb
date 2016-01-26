97dafce89e92da679416f01ec212d365bdd3d4f9
Michael Ries <michael@riesd.com>
2016-01-26 14:54:09 -0700

    integration spec that covers the bug
    There is an existing bug in ActionSubscriber where the user can setup a route with `action = :some_method`,
    but when messages are received it will use the last part of the `routing_key` instead of the action that was
    specific when the user was drawing their routes.

---
+++ spec/integration/custom_actions_spec.rb
@@ -0,0 +1,24 @@
+class CustomActionSubscriber < ActionSubscriber::Base
+  def wat
+    $messages << payload
+  end
+end
+
+describe "A subscriber with a custom action", :integration => true do
+  let(:draw_routes) do
+    ::ActionSubscriber.draw_routes do
+      route ::CustomActionSubscriber, :wat,
+        :queue => "unrelated_to_the_action",
+        :routing_key => "*.javascript_framework"
+    end
+  end
+
+  it "routes the message to the selected action" do
+    ::ActionSubscriber.auto_subscribe!
+    ::ActionSubscriber::Publisher.publish("react.javascript_framework", "Another?!?!", "events")
+
+    verify_expectation_within(2.0) do
+      expect($messages).to eq(Set.new(["Another?!?!"]))
+    end
+  end
+end
