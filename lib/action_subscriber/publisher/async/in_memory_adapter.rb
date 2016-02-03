381e605dd32d7f85dc2974314dcc5e6ed3467401
Garrett Thornburg <garrett.thornburg@moneydesktop.com>
2016-02-03 13:18:51 -0700

    Refactor and add specs for in-memory async message publiser

--- lib/action_subscriber/publisher/async/in_memory_adapter.rb
+++ lib/action_subscriber/publisher/async/in_memory_adapter.rb
@@ -34,112 +34,117 @@ module ActionSubscriber
             sleep 0.1
           end
         end
-      end
 
-      class Message
-        attr_reader :route, :payload, :exchange_name, :options
+        class Message
+          attr_reader :route, :payload, :exchange_name, :options
 
-        def initialize(route, payload, exchange_name, options)
-          @route = route
-          @payload = payload
-          @exchange_name = exchange_name
-          @options = options
-        end
-      end
-
-      class UnableToPersistMessageError < ::StandardError
-      end
-
-      class AsyncQueue
-        include ::ActionSubscriber::Logging
-
-        attr_reader :consumer, :queue, :supervisor
-
-        if ::RUBY_PLATFORM == "java"
-          NETWORK_ERRORS = [::MarchHare::Exception, ::Java::ComRabbitmqClient::AlreadyClosedException, ::Java::JavaIo::IOException].freeze
-        else
-          NETWORK_ERRORS = [::Bunny::Exception, ::Timeout::Error, ::IOError].freeze
+          def initialize(route, payload, exchange_name, options)
+            @route = route
+            @payload = payload
+            @exchange_name = exchange_name
+            @options = options
+          end
         end
 
-        def initialize
-          @queue = ::Queue.new
-          create_and_supervise_consumer!
+        class UnableToPersistMessageError < ::StandardError
         end
 
-        def push(message)
-          # Default of 1_000_000 messages.
-          if queue.size > ::ActionSubscriber.configuration.async_publisher_max_queue_size
-            # Drop Messages if the queue is full and we were configured to do so.
-            return if ::ActionSubscriber.configuration.async_publisher_drop_messages_when_queue_full
+        class AsyncQueue
+          include ::ActionSubscriber::Logging
 
-            # By default we will raise an error to push the responsibility onto the caller.
-            fail UnableToPersistMessageError, "Queue is full, messages will be dropped."
+          attr_reader :consumer, :queue, :supervisor
+
+          if ::RUBY_PLATFORM == "java"
+            NETWORK_ERRORS = [::MarchHare::Exception, ::Java::ComRabbitmqClient::AlreadyClosedException, ::Java::JavaIo::IOException].freeze
+          else
+            NETWORK_ERRORS = [::Bunny::Exception, ::Timeout::Error, ::IOError].freeze
           end
 
-          queue.push(message)
-        end
+          def initialize
+            @queue = ::Queue.new
+            create_and_supervise_consumer!
+          end
 
-        def size
-          queue.size
-        end
+          def push(message)
+            # Default of 1_000_000 messages.
+            if queue.size > ::ActionSubscriber.configuration.async_publisher_max_queue_size
+              # Drop Messages if the queue is full and we were configured to do so.
+              return if ::ActionSubscriber.configuration.async_publisher_drop_messages_when_queue_full
 
-      private
-
-        def create_and_supervise_consumer!
-          @consumer = create_consumer
-          @supervisor = ::Thread.new do
-            loop do
-              unless consumer.alive?
-                # Why might need to requeue the last message.
-                queue.push(@current_message) if @current_message.present?
-                consumer.kill
-                @consumer = create_consumer
-              end
-
-              # Pause before checking the consumer again.
-              sleep supervisor_interval
+              # By default we will raise an error to push the responsibility onto the caller.
+              fail UnableToPersistMessageError, "Queue is full, messages will be dropped."
             end
+
+            queue.push(message)
           end
-        end
 
-        def create_consumer
-          ::Thread.new do
-            loop do
-              # Write "current_message" so we can requeue should something happen to the consumer. I don't love this, but it's
-              # better than writing my own `#peek' method.
-              @current_message = message = queue.pop
+          def size
+            queue.size
+          end
 
-              begin
-                ::ActionSubscriber::Publisher.publish(message.route, message.payload, message.exchange_name, message.options)
+        private
 
-                # Reset
-                @current_message = nil
-              rescue *NETWORK_ERRORS
-                # Sleep because the connection is down.
-                sleep ::ActionSubscriber::RabbitConnection::NETWORK_RECOVERY_INTERVAL
-                # Requeue and try again.
-                queue.push(message)
-              rescue => unknown_error
-                # Do not requeue the message because something else horrible happened.
-                @current_message = nil
+          def await_network_reconnect
+            sleep ::ActionSubscriber::RabbitConnection::NETWORK_RECOVERY_INTERVAL
+          end
 
-                # Log the error.
-                logger.info unknown_error.class
-                logger.info unknown_error.message
-                logger.info unknown_error.backtrace.join("\n")
+          def create_and_supervise_consumer!
+            @consumer = create_consumer
+            @supervisor = ::Thread.new do
+              loop do
+                unless consumer.alive?
+                  # Why might need to requeue the last message.
+                  queue.push(@current_message) if @current_message.present?
+                  consumer.kill
+                  @consumer = create_consumer
+                end
 
-                # TODO: Find a way to bubble this out of the thread for logging purposes.
-                # Reraise the error out of the publisher loop. The Supervisor will restart the consumer.
-                raise unknown_error
+                # Pause before checking the consumer again.
+                sleep supervisor_interval
               end
             end
           end
-        end
 
-        def supervisor_interval
-          @supervisor_interval ||= begin
-            interval_in_milliseconds = ::ActionSubscriber.configuration.async_publisher_supervisor_interval
-            interval_in_milliseconds / 1000.0
+          def create_consumer
+            ::Thread.new do
+              loop do
+                # Write "current_message" so we can requeue should something happen to the consumer. I don't love this, but it's
+                # better than writing my own `#peek' method.
+                @current_message = message = queue.pop
+
+                begin
+                  ::ActionSubscriber::Publisher.publish(message.route, message.payload, message.exchange_name, message.options)
+
+                  # Reset
+                  @current_message = nil
+                rescue *NETWORK_ERRORS
+                  # Sleep because the connection is down.
+                  await_network_reconnect
+
+                  # Requeue and try again.
+                  queue.push(message)
+                rescue => unknown_error
+                  # Do not requeue the message because something else horrible happened.
+                  @current_message = nil
+
+                  # Log the error.
+                  logger.info unknown_error.class
+                  logger.info unknown_error.message
+                  logger.info unknown_error.backtrace.join("\n")
+
+                  # TODO: Find a way to bubble this out of the thread for logging purposes.
+                  # Reraise the error out of the publisher loop. The Supervisor will restart the consumer.
+                  raise unknown_error
+                end
+              end
+            end
+          end
+
+          def supervisor_interval
+            @supervisor_interval ||= begin
+              interval_in_milliseconds = ::ActionSubscriber.configuration.async_publisher_supervisor_interval
+              interval_in_milliseconds / 1000.0
+            end
           end
         end
       end
