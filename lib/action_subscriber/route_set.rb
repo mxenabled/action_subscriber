20b884048306f5f5b077488aa3c768a81a8d85dc
Dmitry Zenovich <dzenovich@gmail.com>
2016-04-27 19:03:25 +0300

    Update route_set.rb
    Use another configuration parameter for specifying durability of the exchanges.

--- lib/action_subscriber/route_set.rb
+++ lib/action_subscriber/route_set.rb
@@ -26,7 +26,7 @@ module ActionSubscriber
 
     def setup_queue(route)
       channel = ::ActionSubscriber::RabbitConnection.subscriber_connection.create_channel
-      exchange = channel.topic(route.exchange, :durable => route.durable)
+      exchange = channel.topic(route.exchange, :durable => route.exchange_durable)
       queue = channel.queue(route.queue, :durable => route.durable)
       queue.bind(exchange, :routing_key => route.routing_key)
       queue
