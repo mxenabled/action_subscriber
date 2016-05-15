f2a90321dafa4be7ce9d5b10b70638a46d3fc19e
Jason Ayre <jasonayre@gmail.com>
2016-05-15 13:09:26 -0600

    add example of adding/applying middleware stack in routes

--- lib/action_subscriber/middleware/stack.rb
+++ lib/action_subscriber/middleware/stack.rb
@@ -5,7 +5,7 @@ module ActionSubscriber
     class Stack < ::Middleware::Builder
       def forked
         forked_stack = self.class.new(:runner_class => @runner_class)
-        forked_stack.instance_variable_set(:@stack, @stack.deep_dup)
+        forked_stack.instance_variable_set(:@stack, @stack.dup)
         forked_stack
       end
     end
