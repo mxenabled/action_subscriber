ff56991c814fef24b2b30af95c1a62108d3e7936
Jason Ayre <jasonayre@gmail.com>
2016-05-15 10:23:57 -0600

    fix forked stack to preserve args, just deep dup array and set ivar directly as use mangles args

--- lib/action_subscriber/middleware/stack.rb
+++ lib/action_subscriber/middleware/stack.rb
@@ -5,11 +5,7 @@ module ActionSubscriber
     class Stack < ::Middleware::Builder
       def forked
         forked_stack = self.class.new(:runner_class => @runner_class)
-
-        @stack.each do |middleware_args|
-          forked_stack.use(middleware_args.first)
-        end
-
+        forked_stack.instance_variable_set(:@stack, @stack.deep_dup)
         forked_stack
       end
     end
