9ee517176841304770bcd9587a9218d448edf462
Michael Ries <michael@riesd.com>
2016-09-14 08:38:30 -0600

    protect the consumer threads from unhandled exceptions

--- lib/action_subscriber/middleware/error_handler.rb
+++ lib/action_subscriber/middleware/error_handler.rb
@@ -8,10 +8,17 @@ module ActionSubscriber
       end
 
       def call(env)
-        @app.call(env)
-      rescue => error
-        logger.error "FAILED #{env.message_id}"
-        ::ActionSubscriber.configuration.error_handler.call(error, env.to_h)
+        # This insulates the connection thread from errors that are raised by the error_handle or
+        # exceptions that don't fall under StandardError (which are not caught by `rescue => error`)
+        new_thread = ::Thread.new do
+          begin
+            @app.call(env)
+          rescue => error
+            logger.error "FAILED #{env.message_id}"
+            ::ActionSubscriber.configuration.error_handler.call(error, env.to_h)
+          end
+        end
+        ::Thread.pass while new_thread.alive?
       end
     end
   end
