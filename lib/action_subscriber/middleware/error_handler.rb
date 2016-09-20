519228d509c4ed81eea921989cd1c155164d13f7
Michael Ries <michael@riesd.com>
2016-09-19 22:07:29 -0600

    busy-waiting on jobs is expensive, switch back to a rescue block for now

--- lib/action_subscriber/middleware/error_handler.rb
+++ lib/action_subscriber/middleware/error_handler.rb
@@ -8,17 +8,10 @@ module ActionSubscriber
       end
 
       def call(env)
-        # This insulates the connection thread from errors that are raised by the error_handle or
-        # exceptions that don't fall under StandardError (which are not caught by `rescue => error`)
-        new_thread = ::Thread.new do
-          begin
-            @app.call(env)
-          rescue => error
-            logger.error "FAILED #{env.message_id}"
-            ::ActionSubscriber.configuration.error_handler.call(error, env.to_h)
-          end
-        end
-        ::Thread.pass while new_thread.alive?
+        @app.call(env)
+      rescue => error
+        logger.error "FAILED #{env.message_id}"
+        ::ActionSubscriber.configuration.error_handler.call(error, env.to_h)
       end
     end
   end
