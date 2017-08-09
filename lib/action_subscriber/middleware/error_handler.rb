49259c7f38e33831a2d4375d87dc7cf720c2e14e
Garrett Thornburg <film42@gmail.com>
2017-08-09 13:21:39 -0400

    Rescue any errors that might be raised out of the error handler
    Due to the way MarchHare and its java lib handle errors, it's important
    to not re-raise errors that are already handled.

--- lib/action_subscriber/middleware/error_handler.rb
+++ lib/action_subscriber/middleware/error_handler.rb
@@ -11,7 +11,16 @@ module ActionSubscriber
         @app.call(env)
       rescue => error
         logger.error "FAILED #{env.message_id}"
-        ::ActionSubscriber.configuration.error_handler.call(error, env.to_h)
+
+        # There is more to this rescue than meets the eye. MarchHare's java library will rescue errors
+        # and attempt to close the channel with its default exception handler. To avoid this, we will
+        # stop errors right here. If you want to handle errors, you must do it in the error handler and
+        # it should not re-raise. As a bonus, not killing these threads is better for your runtime :).
+        begin
+          ::ActionSubscriber.configuration.error_handler.call(error, env.to_h)
+        rescue => error
+          logger.error "ActionSubscriber error handler raised error, but should never raise. Error: #{error}"
+        end
       end
     end
   end
