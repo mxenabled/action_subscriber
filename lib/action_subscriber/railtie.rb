55a232e42a6b77c171f7d8237e53ed0ccdea24f7
Brandon Dewitt <brandonsdewitt@gmail.com>
2018-01-12 20:36:25 -0700

    use insert_before/insert_after instead of use for middlewares

--- lib/action_subscriber/railtie.rb
+++ lib/action_subscriber/railtie.rb
@@ -6,8 +6,8 @@ module ActionSubscriber
       require "action_subscriber/middleware/active_record/connection_management"
       require "action_subscriber/middleware/active_record/query_cache"
 
-      ::ActionSubscriber.config.middleware.use ::ActionSubscriber::Middleware::ActiveRecord::ConnectionManagement
-      ::ActionSubscriber.config.middleware.use ::ActionSubscriber::Middleware::ActiveRecord::QueryCache
+      ::ActionSubscriber.config.middleware.insert_after ::ActionSubscriber::Middleware::Decoder, ::ActionSubscriber::Middleware::ActiveRecord::ConnectionManagement
+      ::ActionSubscriber.config.middleware.insert_after ::ActionSubscriber::Middleware::ActiveRecord::ConnectionManagement, ::ActionSubscriber::Middleware::ActiveRecord::QueryCache
     end
   end
 end
