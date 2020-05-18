5464d7fb1d463711df42a32a96ecd54d76690236
Garrett Thornburg <film42@gmail.com>
2020-05-18 16:04:35 -0700

    Add specs to ensure blocked event correctly fire for java

--- spec/lib/action_subscriber/rabbit_connection_spec.rb
+++ spec/lib/action_subscriber/rabbit_connection_spec.rb
@@ -6,20 +6,28 @@ describe ::ActionSubscriber::RabbitConnection do
   before { ActionSubscriber.draw_routes {} }
 
   if ::RUBY_PLATFORM == "java"
-    it "can deliver an on_blocked message" do
+    def trigger_mocked_blocking_event(connection, reason)
+      amqp_message = ::Java::ComRabbitmqClient::AMQP::Connection::Blocked::Builder.new.
+        reason(reason).build
+      amq_command = ::Java::ComRabbitmqClientImpl::AMQCommand.new(amqp_message)
 
+      connection.send(:processControlCommand, amq_command)
     end
   else
-    it "can deliver an on_blocked message" do
-      expect(::ActiveSupport::Notifications).to receive(:instrument).
-        with("connection_blocked.action_subscriber", :reason => reason)
+    def trigger_mocked_blocking_event(connection, reason)
+      connection.send(:handle_frame, 0, ::AMQ::Protocol::Connection::Blocked.new(reason))
+    end
+  end
 
-      described_class.with_connection do |connection|
-        # NOTE: Trigger the receiving of a blocked message from the broker.
-        # It's a bit of a hack but it is a more realistic test without changing
-        # memory alarms.
-        connection.send(:handle_frame, 0, ::AMQ::Protocol::Connection::Blocked.new(reason))
-      end
+  it "can deliver an on_blocked message" do
+    expect(::ActiveSupport::Notifications).to receive(:instrument).
+      with("connection_blocked.action_subscriber", :reason => reason)
+
+    described_class.with_connection do |connection|
+      # NOTE: Trigger the receiving of a blocked message from the broker.
+      # It's a bit of a hack but it is a more realistic test without changing
+      # memory alarms.
+      trigger_mocked_blocking_event(connection, reason)
     end
   end
 end
