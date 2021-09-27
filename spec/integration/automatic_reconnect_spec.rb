ba4962204b37b4eb1749f008a739772177e76602
Shaun Carlson <shaun.carlson@mx.com>
2021-09-27 00:16:20 -0600

    fix indentation weirdness

--- spec/integration/automatic_reconnect_spec.rb
+++ spec/integration/automatic_reconnect_spec.rb
@@ -7,36 +7,36 @@ class GusSubscriber < ActionSubscriber::Base
 end
 
 describe "Automatically reconnect on connection failure", :integration => true, :slow => true do
- let(:draw_routes) do
-   ::ActionSubscriber.draw_routes do
-     default_routes_for GusSubscriber
-   end
- end
- let(:http_client) { RabbitMQ::HTTP::Client.new("http://127.0.0.1:15672") }
- let(:subscriber) { GusSubscriber }
+  let(:draw_routes) do
+    ::ActionSubscriber.draw_routes do
+      default_routes_for GusSubscriber
+    end
+  end
+  let(:http_client) { RabbitMQ::HTTP::Client.new("http://127.0.0.1:15672") }
+  let(:subscriber) { GusSubscriber }
 
- it "reconnects when a connection drops" do
-   ::ActionSubscriber::start_subscribers!
-   ::ActivePublisher.publish("gus.spoke", "First", "events")
-   verify_expectation_within(5.0) do
-     expect($messages).to eq(Set.new(["First"]))
-   end
+  it "reconnects when a connection drops" do
+    ::ActionSubscriber::start_subscribers!
+    ::ActivePublisher.publish("gus.spoke", "First", "events")
+    verify_expectation_within(5.0) do
+      expect($messages).to eq(Set.new(["First"]))
+    end
 
-   close_all_connections!
-   sleep 5.0
-   verify_expectation_within(5.0) do
-     expect(::ActionSubscriber::RabbitConnection.with_connection{|connection| connection.open?}).to eq(true)
-   end
+    close_all_connections!
+    sleep 5.0
+    verify_expectation_within(5.0) do
+      expect(::ActionSubscriber::RabbitConnection.with_connection{|connection| connection.open?}).to eq(true)
+    end
 
-   ::ActivePublisher.publish("gus.spoke", "Second", "events")
-   verify_expectation_within(5.0) do
-     expect($messages).to eq(Set.new(["First", "Second"]))
-   end
- end
+    ::ActivePublisher.publish("gus.spoke", "Second", "events")
+    verify_expectation_within(5.0) do
+      expect($messages).to eq(Set.new(["First", "Second"]))
+    end
+  end
 
- def close_all_connections!
-   http_client.list_connections.each do |conn_info|
-     http_client.close_connection(conn_info.name)
-   end
- end
+  def close_all_connections!
+    http_client.list_connections.each do |conn_info|
+      http_client.close_connection(conn_info.name)
+    end
+  end
 end
--- spec/integration/consumer_cancellation_spec.rb
+++ spec/integration/consumer_cancellation_spec.rb
@@ -8,76 +8,76 @@ class YoloSubscriber < ActionSubscriber::Base
 end
 
 describe "Automatically handles consumer cancellation", :integration => true, :slow => true do
- let(:draw_routes) do
-   ::ActionSubscriber.draw_routes do
-     default_routes_for ::YoloSubscriber
-   end
- end
- let(:http_client) { ::RabbitMQ::HTTP::Client.new("http://127.0.0.1:15672") }
- let(:subscriber) { ::YoloSubscriber }
+  let(:draw_routes) do
+    ::ActionSubscriber.draw_routes do
+      default_routes_for ::YoloSubscriber
+    end
+  end
+  let(:http_client) { ::RabbitMQ::HTTP::Client.new("http://127.0.0.1:15672") }
+  let(:subscriber) { ::YoloSubscriber }
 
- it "resubscribes on cancellation" do
-   ::ActionSubscriber::start_subscribers!
-   ::ActivePublisher.publish("yolo.created", "First", "events")
-   verify_expectation_within(5.0) do
-     expect($messages).to eq(::Set.new(["First"]))
-   end
+  it "resubscribes on cancellation" do
+    ::ActionSubscriber::start_subscribers!
+    ::ActivePublisher.publish("yolo.created", "First", "events")
+    verify_expectation_within(5.0) do
+      expect($messages).to eq(::Set.new(["First"]))
+    end
 
-   consumers = rabbit_consumers.dup
+    consumers = rabbit_consumers.dup
 
-   # Signal a cancellation event to all subscribers.
-   delete_all_queues!
+    # Signal a cancellation event to all subscribers.
+    delete_all_queues!
 
-   # Give consumers a chance to restart.
-   sleep 2.0
+    # Give consumers a chance to restart.
+    sleep 2.0
 
-   expect(rabbit_consumers).to_not eq(consumers)
+    expect(rabbit_consumers).to_not eq(consumers)
 
-   ::ActivePublisher.publish("yolo.created", "Second", "events")
-   verify_expectation_within(5.0) do
-     expect($messages).to eq(Set.new(["First", "Second"]))
-   end
- end
+    ::ActivePublisher.publish("yolo.created", "Second", "events")
+    verify_expectation_within(5.0) do
+      expect($messages).to eq(Set.new(["First", "Second"]))
+    end
+  end
 
- context "when resubscribe on consumer cancellation is disabled" do
-   before do
-     allow(::ActionSubscriber.configuration).to receive(:resubscribe_on_consumer_cancellation).and_return(false)
-   end
+  context "when resubscribe on consumer cancellation is disabled" do
+    before do
+      allow(::ActionSubscriber.configuration).to receive(:resubscribe_on_consumer_cancellation).and_return(false)
+    end
 
-   it "does not resubscribe on cancellation" do
-     ::ActionSubscriber::start_subscribers!
-     ::ActivePublisher.publish("yolo.created", "First", "events")
-     verify_expectation_within(5.0) do
-       expect($messages).to eq(::Set.new(["First"]))
-     end
+    it "does not resubscribe on cancellation" do
+      ::ActionSubscriber::start_subscribers!
+      ::ActivePublisher.publish("yolo.created", "First", "events")
+      verify_expectation_within(5.0) do
+        expect($messages).to eq(::Set.new(["First"]))
+      end
 
-     consumers = rabbit_consumers.dup
+      consumers = rabbit_consumers.dup
 
-     # Signal a cancellation event to all subscribers.
-     delete_all_queues!
+      # Signal a cancellation event to all subscribers.
+      delete_all_queues!
 
-     # Give consumers a chance to restart.
-     sleep 2.0
+      # Give consumers a chance to restart.
+      sleep 2.0
 
-     # Verify the consumers did not change.
-     expect(rabbit_consumers).to eq(consumers)
+      # Verify the consumers did not change.
+      expect(rabbit_consumers).to eq(consumers)
 
-     ::ActivePublisher.publish("yolo.created", "Second", "events")
+      ::ActivePublisher.publish("yolo.created", "Second", "events")
 
-     # Force sleep 2 seconds to ensure a resubscribe did not happen and messages were not processed.
-     sleep 2.0
-     expect($messages).to eq(Set.new(["First"]))
-   end
- end
+      # Force sleep 2 seconds to ensure a resubscribe did not happen and messages were not processed.
+      sleep 2.0
+      expect($messages).to eq(Set.new(["First"]))
+    end
+  end
 
- def rabbit_consumers
-   route_set = ::ActionSubscriber.send(:route_set)
-   route_set.try(:bunny_consumers) || route_set.try(:march_hare_consumers)
- end
+  def rabbit_consumers
+    route_set = ::ActionSubscriber.send(:route_set)
+    route_set.try(:bunny_consumers) || route_set.try(:march_hare_consumers)
+  end
 
- def delete_all_queues!
-   http_client.list_queues.each do |queue|
-     http_client.delete_queue(queue.vhost, queue.name)
-   end
- end
+  def delete_all_queues!
+    http_client.list_queues.each do |queue|
+      http_client.delete_queue(queue.vhost, queue.name)
+    end
+  end
 end
